d5e4daa7e91d7bdb93340c9bf6bf03626e712f8c
aion0721 <rudolf.aion@gmail.com>
2025-04-29 21:25:45 +0900

    事前準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <h2>My First App</h2>
-      <h2>Addline_1</h2>
-      <h2>Addline_2</h2>
     </>
   );
 }
